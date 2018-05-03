3048af9c7082c2f251ea851a906f3b890e1e4ca8
Charles <Charles@EdesignerBrasil>
2018-05-02 21:04:50 -0300

    Adding menu

--- src/mplayer/Main.java
+++ src/mplayer/Main.java
@@ -1,22 +1,33 @@
 package mplayer;
-	
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import mplayer.view.Player;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-	Player player = new Player("file:///C:/Users/Charles/eclipse-workspace/MPlayer/media/26.mp4");
-	Scene scene = new Scene(player, 720, 510, Color.BLACK);
-	primaryStage.setScene(scene);
-	primaryStage.show();
+
+		MenuItem open = new MenuItem("Abrir");
+		Menu file = new Menu("Arquivos");
+		MenuBar menu = new MenuBar();
+		
+		file.getItems().add(open);
+		menu.getMenus().add(file);
+		
+		Player player = new Player("file:///C:/Users/Charles/eclipse-workspace/MPlayer/media/26.mp4");
+		player.setTop(menu);
+		
+		Scene scene = new Scene(player, 720, 510, Color.BLACK);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
